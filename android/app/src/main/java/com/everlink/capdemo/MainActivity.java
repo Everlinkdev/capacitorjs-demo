af8b9707120fb378b268a707a09ed016d92089f2
Nathan Lewis <112nathan112@gmail.com>
2022-01-04 09:32:19 +0000

    Capacitor added

---
+++ android/app/src/main/java/com/everlink/capdemo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.everlink.capdemo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div display="flex" flex-direction="column">
+      <div display="flex" flex-direction="row">
+        <button id="recordButton">Record</button>
+        <button id="stopButton">Stop</button>
+        <button id="playButton">Play</button>
+        <button id="playConstant">Play From token</button>
+        <button id="stopPlaying">Stop Playing</button>
+      </div>
+      <div display="flex" flex-direction="row">
+        <button id="newToken">Get New Token</button>
+        <button id="saveSounds">Save sounds</button>
+        <button id="clearSounds">Clear sounds</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
@@ -21,8 +31,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  background: black;
+  color:white;
+  font-weight:500;
+  padding: 10px 25px 10px 25px;
+  margin:5px;
+  border: 0px solid black;
 }
 </style>
