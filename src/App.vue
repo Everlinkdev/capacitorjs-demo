b8c2e7d61929fbc80d0285ca8a65eba21f7a2d8e
Nathan Lewis <112nathan112@gmail.com>
2022-06-22 11:25:24 +0100

    Calling functions on click

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,33 @@
   <div id="app">
     <div display="flex" flex-direction="column">
       <div display="flex" flex-direction="row">
-        <button id="recordButton">Record</button>
-        <button id="stopButton">Stop</button>
-        <button id="playButton">Play</button>
-        <button id="playConstant">Play From token</button>
-        <button id="stopPlaying">Stop Playing</button>
+        <button @click="startListening" id="recordButton">Record</button>
+        <button @click="stopListening" id="stopButton">Stop</button>
+        <button @click="startEmitting" id="playButton">Play</button>
+        <button @click="startEmittingToken" id="playConstant">
+          Play From token
+        </button>
+        <button @click="stopEmitting" id="stopPlaying">Stop Playing</button>
       </div>
       <div display="flex" flex-direction="row">
         <button @click="newToken" id="newToken">Get New Token</button>
-        <button id="saveSounds">Save sounds</button>
-        <button id="clearSounds">Clear sounds</button>
+        <button @click="saveTokens" id="saveSounds">Save sounds</button>
+        <button @click="clearTokens" id="clearSounds">Clear sounds</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 import { Plugins } from "@capacitor/core";
 const { EverlinkPlugin } = Plugins;
 
 var myPluginEventListener;
 var myPluginEventListener2;
 
-
 export default {
-  name: 'App',
-  
+  name: "App",
+
   methods: {
     startListening() {
       //to start listening for a code call:
@@ -48,47 +48,53 @@ export default {
     },
     startEmittingToken() {
       //to start emitting the audio code of a token:
-      EverlinkPlugin.everlinkPlayToken({ token: 'exampleToken12345', isOffline: false });
+      EverlinkPlugin.everlinkPlayToken({
+        token: "exampleToken12345",
+        isOffline: false,
+      });
     },
     newToken() {
       //generate a new user token to save in your database
       EverlinkPlugin.everlinkNewToken();
-      alert('token');
     },
     playVolume() {
-      //set the volume 
+      //set the volume
       EverlinkPlugin.everlinkPlayVolume({ volume: 0.6, useLoudspeaker: false });
     },
     saveTokens() {
-      //save tokens and their audiocodes 
-      const tokensArray = ['exampleToken12345', 'exampleToken12346']
+      //save tokens and their audiocodes
+      const tokensArray = ["exampleToken12345", "exampleToken12346"];
       EverlinkPlugin.everlinkSaveSounds({ tokens: tokensArray });
     },
     clearTokens() {
-      //delete saved tokens and their audiocodes 
+      //delete saved tokens and their audiocodes
       EverlinkPlugin.everlinkClearSounds();
-    }
+    },
   },
 
   created: function () {
-     myPluginEventListener = EverlinkPlugin.addListener("onAudioCodeReceived", (result) => {
-      const token = JSON.stringify(result.detectedToken);
-      alert(token);
-    });
+    myPluginEventListener = EverlinkPlugin.addListener(
+      "onAudioCodeReceived",
+      (result) => {
+        const token = JSON.stringify(result.detectedToken);
+        alert(token);
+      }
+    );
 
-     myPluginEventListener2 = EverlinkPlugin.addListener("onMyTokenGenerated", (result) => {
-      const token = JSON.stringify(result.newToken);
-      alert(token);
-    });
+    myPluginEventListener2 = EverlinkPlugin.addListener(
+      "onMyTokenGenerated",
+      (result) => {
+        const token = JSON.stringify(result.newToken);
+        alert(token);
+      }
+    );
   },
 
   destroyed: function () {
     myPluginEventListener.remove();
     myPluginEventListener2.remove();
-  }
-  
-}
-
+  },
+};
 </script>
 
 <style>
@@ -101,10 +107,10 @@ export default {
 }
 button {
   background: black;
-  color:white;
-  font-weight:500;
+  color: white;
+  font-weight: 500;
   padding: 10px 25px 10px 25px;
-  margin:5px;
+  margin: 5px;
   border: 0px solid black;
 }
 </style>
